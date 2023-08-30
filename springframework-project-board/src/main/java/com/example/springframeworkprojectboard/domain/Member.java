23f5d9c850b8b5f33bb2a80d64229ba415e501a9
Seong-Hoon-Lim <hooney200@gmail.com>
2023-08-30 21:24:13 +0900

    #10 - 도메인 수정
    MyBatis 사용으로 테이블과 도메인 클래스의 각 컬럼과 필드의 타입 및 갯수 일치 해야되므로 도메인 수정

--- springframework-project-board/src/main/java/com/example/springframeworkprojectboard/domain/Member.java
+++ springframework-project-board/src/main/java/com/example/springframeworkprojectboard/domain/Member.java
@@ -11,14 +11,14 @@ import java.time.LocalDateTime;
 @Builder
 public class Member {
     private long id;
-    private String memberId;
+    private String account;
     private String password;
     private String name;
     private String gender;
     private String birth;
     private String email;
     private String phone;
-    private String zipcode;     //우편번호
+    private String zipcode; //우편번호
     private String addr1;
     private String addr2;   //상세주소
     private LocalDateTime createdAt;
