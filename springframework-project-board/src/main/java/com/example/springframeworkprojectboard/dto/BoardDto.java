415874207015bf56298a12722840b5c99d6b98a3
Seong-Hoon-Lim <hooney200@gmail.com>
2023-08-31 20:49:25 +0900

    #12 - 매핑을 위한 Dto 생성
    도메인 클래스와 매핑을 위한 BoardDto 생성

---
+++ springframework-project-board/src/main/java/com/example/springframeworkprojectboard/dto/BoardDto.java
@@ -0,0 +1,25 @@
+package com.example.springframeworkprojectboard.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+@Builder
+public class BoardDto {
+    private long id;
+    private long memberId;
+    private String memberName;
+    private String title;
+    private String content;
+    private int hit;
+    private String ip;
+    private int rippleCnt;    //게시판에 등록된 댓글의 갯수
+    private String fileName;  //파일명
+    private long fileSize;    //파일크기
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
