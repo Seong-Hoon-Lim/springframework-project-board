44e1033f3a3cf670bf132bf8106a761906c130f6
Seong-Hoon-Lim <hooney200@gmail.com>
2023-09-05 01:03:36 +0900

    #18 - 회원 관리 관련 Mapper 구현 및 테스트
    * 회원 생성 테스트
    * 회원 조회 테스트
    * 회원 수정 테스트
    * 회원 삭제 테스트

--- springframework-project-board/src/main/java/com/example/springframeworkprojectboard/mapper/MemberMapper.java
+++ springframework-project-board/src/main/java/com/example/springframeworkprojectboard/mapper/MemberMapper.java
@@ -9,11 +9,54 @@ import java.sql.SQLException;
 public interface MemberMapper {
 
     /**
-     * 회원 저장
+     * 회원 계정 DB 중복 검증
+     * @param account
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    boolean isExistByAccount(String account) throws SQLException, ClassNotFoundException;
+
+    /**
+     * 회원 DB 저장
      * @param member
      * @throws SQLException
      * @throws ClassNotFoundException
      */
     void save(Member member) throws SQLException, ClassNotFoundException;
+
+    /**
+     * 고유번호로 회원 DB 조회
+     * @param memberId
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    Member findMemberById(long memberId) throws SQLException, ClassNotFoundException;
+
+    /**
+     * 계정으로 회원 DB 조회
+     * @param account
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    Member findMemberByAccount(String account) throws SQLException, ClassNotFoundException;
+
+    /**
+     * 회원 DB 수정
+     * @param member
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    void update(Member member) throws SQLException, ClassNotFoundException;
+
+    /**
+     * 회원 DB 삭제
+     * @param memberId
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    void deleteMemberByMemberId(long memberId) throws SQLException, ClassNotFoundException;
 
 }
