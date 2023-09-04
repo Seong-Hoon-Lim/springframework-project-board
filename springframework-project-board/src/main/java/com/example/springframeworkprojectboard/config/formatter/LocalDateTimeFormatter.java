2002d5615f9beb716f396e7d924fad262685f5c9
Seong-Hoon-Lim <hooney200@gmail.com>
2023-09-04 13:40:07 +0900

    #16 - Formatter 수정
    데이터 타입 LocalDateTime 과 게시판의 날짜 검색 조건의 포맷 중 에러 발생으로 문제해결을 위해 날짜 검색 조건에 맞춰 formatter일부 수정

--- springframework-project-board/src/main/java/com/example/springframeworkprojectboard/config/formatter/LocalDateTimeFormatter.java
+++ springframework-project-board/src/main/java/com/example/springframeworkprojectboard/config/formatter/LocalDateTimeFormatter.java
@@ -3,7 +3,9 @@ package com.example.springframeworkprojectboard.config.formatter;
 import org.springframework.format.Formatter;
 
 import java.text.ParseException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -11,7 +13,11 @@ public class LocalDateTimeFormatter implements Formatter<LocalDateTime> {
 
     @Override
     public LocalDateTime parse(String text, Locale locale) throws ParseException {
-        return LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        if (text.length() == 10) { // Only Date
+            return LocalDateTime.of(LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd")), LocalTime.MIN);
+        } else {
+            return LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        }
     }
 
     @Override
@@ -19,3 +25,4 @@ public class LocalDateTimeFormatter implements Formatter<LocalDateTime> {
         return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(object);
     }
 }
+
