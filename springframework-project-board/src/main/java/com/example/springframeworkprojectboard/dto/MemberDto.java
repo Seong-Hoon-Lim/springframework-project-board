2b5228994f4fb4701055bd65505bfd1766b9e097
Seong-Hoon-Lim <hooney200@gmail.com>
2023-09-05 23:56:28 +0900

    #24 - Member 도메인, Dto 수정
    고유 번호 필드 타입 일반 자료형으로 자바스크립트 레이어에서 파싱 에러 등 문제 발생으로 Long 타입 래퍼클래스 형태로 변경

--- springframework-project-board/src/main/java/com/example/springframeworkprojectboard/dto/MemberDto.java
+++ springframework-project-board/src/main/java/com/example/springframeworkprojectboard/dto/MemberDto.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDto {
-    private long id;
+    private Long id;
     private String account;
     private String password;
     private String name;
