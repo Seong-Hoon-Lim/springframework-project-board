402aa9c3aee50d2b4e0dcb147372c64a4ba0f062
Seong-Hoon-Lim <hooney200@gmail.com>
2023-09-05 14:53:37 +0900

    #22 - 회원 관리 관련 컨트롤러 구현 및 MockMvc 테스트
    *회원 관리 관련 GET 매핑 컨트롤러 구현
     MockMvc 를 활용한 테스트
    
    * 회원 관리 관련 POST 매핑 컨트롤러 구현
     MockMvc 를 활용한 테스트

---
+++ springframework-project-board/src/main/java/com/example/springframeworkprojectboard/controller/MemberController.java
@@ -0,0 +1,119 @@
+package com.example.springframeworkprojectboard.controller;
+
+import com.example.springframeworkprojectboard.dto.MemberDto;
+import com.example.springframeworkprojectboard.service.MemberServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+@Log4j2
+public class MemberController {
+
+    private final MemberServiceImpl memberService;
+
+    @PostMapping(value = "/checkAccount")
+    public void checkDuplicateAccount(HttpServletRequest req, HttpServletResponse resp) {
+        log.info("MemberController: POST - checkDuplicateAccount()");
+        resp.setContentType("application/json");
+        String account = req.getParameter("account");
+
+        try {
+            String result = memberService.hasDuplicateMember(account) ? "true" : "false";
+            String jsonResult = String.format("{\"result\":\"%s\"}", result);
+            resp.getWriter().print(jsonResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/member_register")
+    public String showRegisterForm() {
+        return "member/member_register";
+    }
+
+    @PostMapping("/member_register")
+    public String createMember(@ModelAttribute MemberDto memberDto,
+                               HttpServletRequest req,
+                               Model model) {
+        log.info("MemberController: POST - createMember()");
+        String year = req.getParameter("birthyy");
+        String month = req.getParameterValues("birthmm")[0];
+        String day = req.getParameter("birthdd");
+        memberDto.setBirth(year + "/" + month + "/" + day);
+
+        String mail1 = req.getParameter("mail1");
+        String mail2 = req.getParameterValues("mail2")[0];
+        memberDto.setEmail(mail1 + "@" + mail2);
+
+        model.addAttribute("member", memberDto);
+        try {
+            if (!memberService.hasDuplicateMember(memberDto.getAccount())) {
+                memberService.registerMember(memberDto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/member/login";
+    }
+
+    @GetMapping({"/member_view", "/member_modify"})
+    public void showMember(long memberId, HttpServletRequest req, Model model) {
+        log.info("MemberController: GET - showMember()");
+        HttpSession session = req.getSession();
+        try {
+            MemberDto memberDto = memberService.getMember(memberId);
+            model.addAttribute("member", memberDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostMapping("/member_modify")
+    public String modifyMember(@ModelAttribute MemberDto memberDto,
+                               HttpServletRequest req,
+                               Model model) {
+        log.info("MemberController: POST - modifyMember()");
+        String year = req.getParameter("birthyy");
+        String month = req.getParameterValues("birthmm")[0];
+        String day = req.getParameter("birthdd");
+        memberDto.setBirth(year + "/" + month + "/" + day);
+
+        String mail1 = req.getParameter("mail1");
+        String mail2 = req.getParameterValues("mail2")[0];
+        memberDto.setEmail(mail1 + "@" + mail2);
+
+        model.addAttribute("member", memberDto);
+        try {
+            if (!memberService.hasDuplicateMember(memberDto.getAccount())) {
+                memberService.modifyMember(memberDto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/member/member_view";
+
+    }
+
+    @PostMapping("/member_remove")
+    public String removeMember(long memberId, HttpServletRequest req) {
+        log.info("MemberController: POST - removeMember()");
+        try {
+            memberService.removeMember(memberId);
+            req.getSession().invalidate();
+            return "redirect:/board/board_list";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "member/member_modify";
+    }
+}
